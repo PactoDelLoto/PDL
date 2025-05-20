2dd5d616aeaeb177aca1cb8e929fb78190f14b7b
manuelreinaj <manuelreinaj@gmail.com>
2025-05-20 15:14:31 +0200

    Cambio properties

--- controllers/properties.js
+++ controllers/properties.js
@@ -1,8 +1,8 @@
 window.APP_PROPERTIES = {
     //Direccion de github pages
-    //MAIL_REDIRECT_TO: 'https://pactodelloto.github.io/PDL/resources/views/confirm-email.html'
+    MAIL_REDIRECT_TO: 'https://pactodelloto.github.io/PDL/resources/views/confirm-email.html'
     
     //Direccion local de la vista de confirmación de email
-    EMAIL_REDIRECT_TO: 'http://localhost:5500/resources/views/confirm-email.html'
+    //EMAIL_REDIRECT_TO: 'http://localhost:5500/resources/views/confirm-email.html'
   // Cambia esta URL según el entorno (por ejemplo, en GitHub Pages pon la URL pública)
 };
